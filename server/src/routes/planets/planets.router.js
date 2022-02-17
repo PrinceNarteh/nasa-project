22dc1d1f489a062364677457452c22bf169d2f12
Prince Narteh <prinarteh@gmail.com>
2022-02-17 21:00:35 +0000

    setup server

---
+++ server/src/routes/planets/planets.router.js
@@ -0,0 +1,8 @@
+const { Router } = require("express");
+const { getAllPlanets } = require("./planets.controller");
+
+const planetsRouter = Router();
+
+planetsRouter.get("/", getAllPlanets);
+
+module.exports = planetsRouter;
